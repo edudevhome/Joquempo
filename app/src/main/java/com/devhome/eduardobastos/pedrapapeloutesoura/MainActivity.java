60495d8d0dc260f8ee2bdd8702913c2dc366af47
edudevhome <com.devhome.eduardobastos@gmail.com>
2019-12-12 20:44:24 -0300

    add Botão limpar. add Placar. add AdMob banner nativo. correção do layout.

--- app/src/main/java/com/devhome/eduardobastos/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/devhome/eduardobastos/pedrapapeloutesoura/MainActivity.java
@@ -1,21 +1,57 @@
 package com.devhome.eduardobastos.pedrapapeloutesoura;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+//Classes ADS AdMob
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+
+
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textViewResult;
+    private Button buttonLimpar;
+    private AdView mAdView;
+    private TextView textViewApp, textViewUser;
+    private long scoreApp, scoreUser = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView =  findViewById(R.id.imageViewResultado);
+        textViewResult = findViewById(R.id.textViewEscolhaUser);
+        buttonLimpar = findViewById(R.id.button);
+        textViewApp = findViewById(R.id.textViewScoreApp);
+        textViewUser = findViewById(R.id.textViewScoreUser);
+
+
+        //Configurando Banner Nativo1
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
+
+
 
     public void opcaoPedra(View view) {
 
@@ -60,17 +96,22 @@ public class MainActivity extends AppCompatActivity {
 
                 if (escolhaApp == escolhaUsuario) {
 
-                    texto.setText("Empate técnico");
+                    texto.setText("Empate técnico, ninguém pontuou!");
                 }
 
                     if(escolhaUsuario == "papel"){
 
                         texto.setText("Você venceu!  :D");
+                        scoreUser++;
+                        textViewUser.setText(""+scoreUser);
                     }
 
                         if(escolhaUsuario == "tesoura"){
 
                             texto.setText("Você Perdeu!  :(");
+                            scoreApp ++;
+                            textViewApp.setText(""+scoreApp);
+
                         }
 
 
@@ -84,18 +125,22 @@ public class MainActivity extends AppCompatActivity {
 
                 if (escolhaApp == escolhaUsuario) {
 
-                    texto.setText("Empate técnico");
+                    texto.setText("Empate técnico, ninguém pontuou!");
 
                 }
 
                 if(escolhaUsuario == "tesoura"){
 
                     texto.setText("Você venceu!  :D");
+                    scoreUser ++;
+                    textViewUser.setText(""+scoreUser);
                 }
 
                 if(escolhaUsuario == "pedra"){
 
                     texto.setText("Você Perdeu!  :(");
+                    scoreApp ++;
+                    textViewApp.setText(""+scoreApp);
                 }
                 break;
             case "tesoura":
@@ -103,23 +148,43 @@ public class MainActivity extends AppCompatActivity {
 
                 if (escolhaApp == escolhaUsuario) {
 
-                    texto.setText("Empate técnico");
+                    texto.setText("Empate técnico, ninguém pontuou!");
 
                 }
 
                 if(escolhaUsuario == "pedra"){
 
                     texto.setText("Você venceu!  :D");
+                    scoreUser ++;
+                    textViewUser.setText(""+scoreUser);
                 }
 
                 if(escolhaUsuario == "papel"){
 
                     texto.setText("Você Perdeu!  :(");
+                    scoreApp ++;
+                    textViewApp.setText(""+scoreApp);
                 }
                 break;
 
 
         }
+
+
+    }
+
+    public void limpar(View view){
+
+        imageView.setImageResource(R.drawable.padrao);
+        textViewResult.setText("Escolha a sua arma:");
+        textViewApp.setText("0");
+        textViewUser.setText("0");
+        scoreUser = 0;
+        scoreApp = 0;
+
+        Toast.makeText(MainActivity.this,
+                "Campos limpos com sucesso!",
+                Toast.LENGTH_SHORT).show();
 
 
     }
