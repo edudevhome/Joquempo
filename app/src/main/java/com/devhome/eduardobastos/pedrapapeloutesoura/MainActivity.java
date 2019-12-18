b27945b04d0db1170ea0a36efb524bbce2f575cc
edudevhome <com.devhome.eduardobastos@gmail.com>
2019-12-18 16:07:37 -0300

    add Botão limpar. add Placar. add AdMob banner nativo. correção do layout.

--- app/src/main/java/com/devhome/eduardobastos/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/devhome/eduardobastos/pedrapapeloutesoura/MainActivity.java
@@ -1,13 +1,10 @@
 package com.devhome.eduardobastos.pedrapapeloutesoura;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
